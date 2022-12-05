44ee824a1c6cff5add0f25e280c3301bdf6cc98a
baruce <93377029+Barucee@users.noreply.github.com>
2022-12-05 23:45:29 +0100

    Update OCR.py
    small change

--- OCR/OCR.py
+++ OCR/OCR.py
@@ -1,7 +1,7 @@
 import easyocr as ocr  #OCR
 import streamlit as st  #Web App
 from PIL import Image #Image Processing
-import numpy as np #Image Processing 
+import numpy as np 
 
 #title
 st.title("Easy OCR - Extract Text from Images")
